eb31c7ad8ef74ea40219438327d60d8a1c8ed21d
Firstname Lastname <e-mail address>
2022-09-17 15:47:18 -0700

    Created turtle initializer

---
+++ player.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("turtle")
+        self.goto(0, -280)
+        self.setheading(90)
+
+    def move_player(self):
+        self.forward(10)
